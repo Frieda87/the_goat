0cd3bdf497c9901813e758a2ad2c4ba574cf94e7
Frieda Land <friederike.di.land@gmail.com>
2020-08-04 08:56:43 +0200

    FT now checks whether we can input a to-do-list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,10 +19,35 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         # Also gives info on what was actually found if not the right one
         self.assertIn('To-Do', self.browser.title)
-        # Always fails, kind of a reminder
-        self.fail('Finish the test!') 
+        # Selenium inbuilt methods to examine browser page
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She is invited  to enter a to-do item straight away...
+        # She is invited  to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby)
+        # is tying fly-fising lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1:  Buy peacock feathers" as an item in a to-do list table
+
+        # Key class lets us send special keys like ENTER
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == 'Buy peacock feathers' for row in rows))
+
+        # There is still a text box inviting her to add another item. She 
+        # enters "User peacock feathers to make a fly" (Edith is very 
+        # methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list...
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
